97083fdf8ae8915a15646640695b16aacd973ff0
Dejavu-Jamaisvu <aiwork4242@gmail.com>
2023-02-01 09:50:23 +0900

    222

---
+++ U-02.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+. function.sh
+
+BAR
+
+CODE [U-02] 패스워드 복잡성 설정
+
+cat << EOF >> $result
+
+[양호]: 패스워드 최소길이 8자리 이상, 영문·숫자·특수문자 최소 입력 기능이 설정된 경우
+
+[취약]: 패스워드 최소길이 8자리 이상, 영문·숫자·특수문자 최소 입력 기능이 설정된 경우
+
+EOF
+
+BAR
+
+# 확인필요
+
+# 원본 파일을 백업
+cp /etc/pam.d/system-auth /etc/pam.d/system-auth.bak
+
+# 파일 내용 변경
+echo -e "auth required /lib/security/pam_tally.so deny=5 unlock_time=120\nno_magic_root\naccount required /lib/security/pam_tally.so no_magic_root reset" > /etc/pam.d/system-auth
+
+
+
+cat $result
+
+echo ; echo
---
+++ U-03.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+. function.sh
+
+BAR
+
+CODE [U-03] 패스워드 파일 보호
+
+cat << EOF >> $result
+
+[양호]: 계정 잠금 임계값이 10회 이하의 값으로 설정되어 있는 경우
+
+[취약]: 계정 잠금 임계값이 설정되어 있지 않거나, 10회 이하의 값으로 설정되지 않은 경우
+
+EOF
+
+BAR
+
+
+
+# 원본 파일을 백업
+cp /etc/pam.d/system-auth /etc/pam.d/system-auth.bak
+
+# 파일 내용 변경
+echo -e "auth required /lib/security/pam_tally.so deny=5 unlock_time=120\nno_magic_root\naccount required /lib/security/pam_tally.so no_magic_root reset" > /etc/pam.d/system-auth
+
+
+cat $result
+
+echo ; echo
