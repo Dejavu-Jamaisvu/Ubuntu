663014794891c624a61f5976eb09033997f3b5ad
Dejavu-Jamaisvu <aiwork4242@gmail.com>
2023-02-01 15:26:11 +0900

    0201-0526

--- patch.sh
+++ patch.sh
@@ -4,7 +4,7 @@
 
 TMP1=`SCRIPTNAME`.log
 
-> $TMP1
+> $TMP1 
 
 . U-01.sh
 
@@ -12,7 +12,7 @@ TMP1=`SCRIPTNAME`.log
 
 . U-03.sh
 
-#. U-04.sh
+. U-04.sh
 
 . U-05.sh
 
@@ -64,13 +64,13 @@ TMP1=`SCRIPTNAME`.log
 
 . U-29.sh
 
-#. U-30.sh
+. U-30.sh
 
 . U-31.sh
 
 . U-32.sh
 
-#. U-33.sh
+. U-33.sh
 
 . U-34.sh
 
@@ -88,7 +88,7 @@ TMP1=`SCRIPTNAME`.log
 
 . U-41.sh
 
-#. U-42.sh
+. U-42.sh
 
 . U-43.sh
 
@@ -105,3 +105,4 @@ TMP1=`SCRIPTNAME`.log
 . U-49.sh
 
 . U-50.sh
+
